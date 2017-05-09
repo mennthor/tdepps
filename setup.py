f1ba083b2e79812ae49bc53e2c2b7e1865684499
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-05-09 13:17:33 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tdepps',
+      version='0.1',
+      description='Time Dependent Point Source Analysis',
+      author='Thorben Menne',
+      author_email='thorben.menne@tu-dortmund.de',
+      url='github.com/mennthor/tdepps',
+      packages=['tdepps'],
+      )
