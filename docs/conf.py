afc54dd3ad4ad839bdce36aafbdbdba39d11ff61
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-05-11 20:56:44 -0400

    Removed hardcoded path and unsynced folders

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@
 import os
 import sys
 import sphinx_rtd_theme
-sys.path.insert(0, '/Users/tmenne/git/misc/tdepps/tdepps')
 
 
 # -- General configuration ------------------------------------------------
@@ -40,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -105,7 +104,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
