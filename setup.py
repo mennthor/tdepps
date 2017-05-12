55d83a266b8c5ca258445b360313b571b18726da
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-05-11 20:19:49 -0400

    Removed dependency_links

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 
-req = ['numpy', 'scipy', 'scikit-learn', 'sphinx', 'docrep', 'anapymods3']
-dep_links = ['http://github.com/user/python_modules3/tarball/' +
-             'master#egg=anapymods3-0.1']
-
-
 setup(name='tdepps',
       version='0.1',
       description='Time Dependent Point Source Analysis',
@@ -12,6 +7,6 @@ setup(name='tdepps',
       author_email='thorben.menne@tu-dortmund.de',
       url='github.com/mennthor/tdepps',
       packages=['tdepps'],
-      install_requires=req,
-      dependency_links=dep_links,
+      install_requires=['numpy', 'scipy', 'scikit-learn', 'docrep'],
       )
+
