db8f0f900f28cfef6d91d7f6b4a20cc71fda17b9
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-05-19 11:28:30 +0200

    Added sinus rate function with fixed 1yr period

--- tdepps/rate_function.py
+++ tdepps/rate_function.py
@@ -34,8 +34,7 @@ class RateFunction(object):
         t : array-like, shape (n_samples)
             MJD times of experimental data.
         pars : tuple, optional
-            Parameters for the fit function. If not None the local parameters
-            are prefered over the global ones. (default: None)
+            Further parameters `fun` depends on.
 
         Returns
         -------
@@ -58,13 +57,12 @@ class RateFunction(object):
         trange : array-like, shape(2)
             Time window `[t0, t1]` in seconds around the source time t.
         pars : tuple, optional
-            Parameters for the fit function. If not None the local parameters
-            are prefered over the global ones. (default: None)
+            Further parameters `fun` depends on.
 
         Returns
         -------
         integral : float
-            Integral of `self.fun` within tiem windows `trange`.
+            Integral of `self.fun` within time windows `trange`.
         """
         raise NotImplementedError("RateFunction is an interface.")
 
@@ -84,8 +82,7 @@ class RateFunction(object):
         trange : array-like, shape(2)
             Time window `[t0, t1]` in seconds around the source time t.
         pars : tuple, optional
-            Parameters for the fit function. If not None the local parameters
-            are prefered over the global ones. (default: None)
+            Further parameters `fun` depends on.
         n_samples : int
             Number of events to sample. (default: 1)
         random_state : RandomState, optional
@@ -180,6 +177,8 @@ class SinusRateFunction(RateFunction):
         Parameters
         ----------
         %(RateFunction.fun.parameters)s
+            Here `pars` are (a, b, c, d) as stated above.
+
 
         Returns
         -------
@@ -196,6 +195,7 @@ class SinusRateFunction(RateFunction):
         Parameters
         ----------
         %(RateFunction.integral.parameters)s
+            Here `pars` are (a, b, c, d) as stated above.
 
         Returns
         -------
@@ -320,3 +320,80 @@ class SinusRateFunction(RateFunction):
         c0 = np.amin(t)
         d0 = np.average(rate, weights=rate_std**2)
         return [a0, b0, c0, d0]
+
+
+class SinusRateFunction1yr(SinusRateFunction):
+    """
+    Sinus Rate Function fixing the period to 1 year.
+
+    Function describes time dependent background rate. Function is a sinus with:
+
+    .. math:: f(t|a,c,d) = a \sin((2\pi/\mathrm{365.25}) (t - c)) + d
+
+    where:
+
+    - a is the Amplitude in Hz
+    - c is the x-offset in MJD
+    - d the y-offset in Hz
+
+    Time is measured in MJD days.
+    """
+    def __init__(self):
+        self._b = 2 * np.pi / 365.25
+        return
+
+    @docs.dedent
+    def fun(self, t, pars):
+        """
+        Returns the rate at a given time in MJD from a sinusodial function.
+
+        Parameters
+        ----------
+        %(RateFunction.fun.parameters)s
+            Here `pars` are (a, c, d) as stated above.
+
+        Returns
+        -------
+        %(RateFunction.fun.returns)s
+        """
+        a, c, d = pars
+        pars = (a, self._b, c, d)  # Just inject the fixed par in the super func
+        return super(SinusRateFunction1yr, self).fun(t, pars)
+
+    @docs.dedent
+    def integral(self, t, trange, pars):
+        """
+        Analytic integral of the sinusodial rate function in interval `trange`.
+
+        Parameters
+        ----------
+        %(RateFunction.integral.parameters)s
+            Here `pars` are (a, c, d) as stated above.
+
+        Returns
+        -------
+        %(RateFunction.integral.returns)s
+        """
+        a, c, d = pars
+        pars = (a, self._b, c, d)  # Just inject the fixed par in the super func
+        return super(SinusRateFunction1yr, self).integral(t, trange, pars)
+
+    def _get_default_seed(self, t, rate, rate_std):
+        """
+        Default seed values derived from data under assumption that it behaves
+        like the fit function.
+
+        Parameters
+        ----------
+        t, rate, rate_std
+            See `SinusRateFunction._get_default_seed`, Parameters
+        Returns
+        -------
+        p0
+            See `SinusRateFunction._get_default_seed`, Parameters.
+            But `b` is fixed here, so only a0, c0 and d0 are seed values.
+        """
+        seed = super(SinusRateFunction1yr, self)._get_default_seed(t, rate,
+                                                                   rate_std)
+        # Drop b0 seed, as it's fixed to 1yr here
+        return [seed[i] for i in [0, 2, 3]]
