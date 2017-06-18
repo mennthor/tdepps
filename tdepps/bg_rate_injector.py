796fb14714c72e7563c49411dd1d01acfbb1b844
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-06-18 00:28:43 -0400

    Added properties on base class level and added new BinnedBGRateInjector taken prebinned rates

--- tdepps/bg_rate_injector.py
+++ tdepps/bg_rate_injector.py
@@ -9,6 +9,7 @@ import os
 import json
 import numpy as np
 from astropy.time import Time as astrotime
+from sklearn.utils import check_random_state
 
 import abc     # Abstract Base Class
 import docrep  # Reuse docstrings
@@ -16,29 +17,113 @@ docs = docrep.DocstringProcessor()
 
 
 class BGRateInjector(object):
-    """
-    Background Rate Injector Base Class
-
-    Samples times of background-like events for a given time frame.
-
-    Classes must implement methods:
-
-    - `fun`
-    - `sample`
-
-    Class object then provides public methods:
-
-    - `fun`
-    - `sample`
-    - `get_nb`
-    """
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
+    @docs.get_sectionsf("BGRateInjector.init", sections=["Parameters"])
+    @docs.dedent
+    def __init__(self, rate_func):
+        """
+        Background Rate Injector Base Class
+
+        Samples times of background-like events for a given time frame.
+
+        Classes must implement methods:
+
+        - `fun`
+        - `sample`
+
+        Class object then provides public methods:
+
+        - `fun`
+        - `sample`
+        - `get_nb`
+
+        Additionally creates new class attributes after calling the fit method:
+
+            - livetime, float: Livetime in days of the given data.
+            - best_pars, tuple: Best fit parameters.
+            - best_estimator, callable: Rate function with `best_pars` plugged
+              in.
+            - best_estimator_integral, callable: Rate function integral with
+              `best_pars` plugged in.
+
+        Parameters
+        ----------
+        rate_func : `rate_function.RateFunction` instance
+            Class defining the function to describe the time dependent
+            background rate. Must provide functions
+            ['fun', 'integral', 'fit', 'sample'].
+
+        Example
+        -------
+        >>> import tdepps.bg_rate_injector as BGRateInj
+        >>>
+        >>> # Make a simple run filter list
+        >>> filter_runs = lambda run: (run["good_i3"] == True)
+        >>>
+        >>> # Chose a rate function and a path to a goodrunlist
+        >>> rate_func = RateFunc.Sinus1yrRateFunction()
+        >>> runlist = "/path/to/goodrunlist.json"
+        >>> runlist_inj = BGRateInj.RunlistBGRateInjector(runlist, filter_runs,
+                                                          rate_func)
+        """
+        self._rate_func = rate_func
+
         self._SECINDAY = 24. * 60. * 60.
-        self.best_pars = None
-        self.best_estimator = None
-        self.best_estimator_integral = None
+        self._livetime = None
+        self._best_pars = None
+        self._best_estimator = None
+        self._best_estimator_integral = None
+
+        return
+
+    @property
+    def rate_func(self):
+        return self._rate_func
+
+    @rate_func.setter
+    def rate_func(self, arg):
+        raise ValueError("`rate_func` can't be set. Create new object instead.")
+
+    @property
+    def livetime(self):
+        if self._livetime is None:
+            raise RuntimeWarning("BGRateInjector has not been fitted yet.")
+        return self._livetime
+
+    @livetime.setter
+    def livetime(self, livetime):
+        raise ValueError("Use `fit` to calculate a new livetime.")
+
+    @property
+    def best_pars(self):
+        if self._best_pars is None:
+            raise RuntimeWarning("BGRateInjector has not been fitted yet.")
+        return self._best_pars
+
+    @best_pars.setter
+    def best_pars(self, best_pars):
+        raise ValueError("Use `fit` to get new best fit parameters.")
+
+    @property
+    def best_estimator(self):
+        if self._best_estimator is None:
+            raise RuntimeWarning("BGRateInjector has not been fitted yet.")
+        return self._best_estimator
+
+    @best_estimator.setter
+    def best_estimator(self, best_estimator):
+        raise ValueError("Use `fit` to get a new best estimator function.")
+
+    @property
+    def best_estimator_integral(self):
+        if self._best_estimator_integral is None:
+            raise RuntimeWarning("BGRateInjector has not been fitted yet.")
+        return self._best_estimator_integral
+
+    @best_estimator_integral.setter
+    def best_estimator_integral(self, best_estimator_integral):
+        raise ValueError("Use `fit` to get a new best estimator integral.")
 
     @docs.get_summaryf("BGRateInjector.fit")
     @docs.get_sectionsf("BGRateInjector.fit",
@@ -51,6 +136,7 @@ class BGRateInjector(object):
 
         Additionally creates new class attributes:
 
+        - livetime, float: Livetime in days of the given data.
         - best_pars, tuple: Best fit parameters.
         - best_estimator, callable: Rate function with `best_pars` plugged in.
         - best_estimator_integral, callable: Rate function integral with
@@ -63,11 +149,6 @@ class BGRateInjector(object):
         """
         pass
 
-    @docs.get_summaryf("BGRateInjector.sample")
-    @docs.get_sectionsf("BGRateInjector.sample",
-                        sections=["Parameters", "Returns"])
-    @docs.dedent
-    @abc.abstractmethod
     def sample(self, t, trange, poisson=True, random_state=None):
         """
         Generate random samples from the fitted model for multiple source event
@@ -98,7 +179,23 @@ class BGRateInjector(object):
             The times in MJD for each sampled srcs. Arrays might be empty, when
             the background expectation is low for small time windows.
         """
-        pass
+        rndgen = check_random_state(random_state)
+
+        # BG expectations are the integrals over each time frames, shape (nsrcs)
+        t, trange = self._prep_t_trange(t, trange)
+        expect = self.get_nb(t, trange)
+        nsrcs = len(t)
+
+        # Get number of actual events to sample times for
+        if poisson:
+            nevents = rndgen.poisson(lam=expect, size=nsrcs)
+        else:  # If one expectation is < 0.5 no event is sampled for that src
+            nevents = np.round(expect).astype(int)
+
+        # Sample all nevents for this trial from the rate function at once
+        return self.rate_func.sample(t, trange, self.best_pars,
+                                     n_samples=nevents,
+                                     random_state=random_state)
 
     def get_nb(self, t, trange):
         """
@@ -163,49 +260,47 @@ class BGRateInjector(object):
         pars : tuple
             Best fit parameters plugged into `fun` and `integral`.
         """
-        self.best_pars = pars
-        self.best_estimator = (lambda t: self.rate_func.fun(t, pars))
-        self.best_estimator_integral = (
+        self._best_pars = pars
+        self._best_estimator = (lambda t: self.rate_func.fun(t, pars))
+        self._best_estimator_integral = (
             lambda t, trange: self.rate_func.integral(t, trange, pars))
 
-        return self.best_pars
+        return self._best_estimator
 
 
 class RunlistBGRateInjector(BGRateInjector):
-    """
-    Runlist Background Rate Injector
+    @docs.dedent
+    def __init__(self, rate_func, runlist, filter_runs):
+        """
+        Runlist Background Rate Injector
 
-    Creates the time depending rate from a given runlist.
-    Parameters are passed to `create_goodrun_dict` method.
+        Creates the time depending rate from a given runlist.
+        Parameters are passed to `create_goodrun_dict` method.
 
-    Parameters
-    ----------
-    runlist : str
-        Path to a valid good run runlist snapshot from [1]_ in JSON format.
-        Must have keys 'latest_snapshot' and 'runs'.
-    filter_runs : function
-        Filter function to remove unwanted runs from the goodrun list.
-        Called as `filter_runs(run)`. Function must operate on a single
-        dictionary argument, with keys:
-        ['good_i3', 'good_it', 'good_tstart', 'good_tstop', 'run', 'reason_i3',
-        'reason_it', 'source_tstart', 'source_tstop', 'snapshot', 'sha'].
-    rate_func : `rate_function.RateFunction` instance
-        Class defining the function to describe the time dependent
-        background rate. Must provide functions
-        ['fun', 'integral', 'fit', 'sample'].
+        Parameters
+        ----------
+        %(BGRateInjector.init.parameters)s
+        runlist : str
+            Path to a valid good run runlist snapshot from [1]_ in JSON format.
+            Must have keys 'latest_snapshot' and 'runs'.
+        filter_runs : function
+            Filter function to remove unwanted runs from the goodrun list.
+            Called as `filter_runs(run)`. Function must operate on a single
+            dictionary argument, with keys:
+            ['good_i3', 'good_it', 'good_tstart', 'good_tstop', 'run', 'sha',
+             'reason_i3', 'reason_it', 'source_tstart', 'source_tstop',
+             'snapshot'].
 
-    Notes
-    -----
-    .. [1] https://live.icecube.wisc.edu/snapshots/
-    """
-    def __init__(self, runlist, filter_runs, rate_func):
-        super(RunlistBGRateInjector, self).__init__()
+        Notes
+        -----
+        .. [1] https://live.icecube.wisc.edu/snapshots/
+        """
+        super(RunlistBGRateInjector, self).__init__(rate_func)
 
         # Create a goodrun list from the JSON snapshot
         runlist = os.path.abspath(runlist)
         self.goodrun_dict = self.create_goodrun_dict(runlist, filter_runs)
 
-        self.rate_func = rate_func
         return
 
     @docs.dedent
@@ -216,6 +311,7 @@ class RunlistBGRateInjector(BGRateInjector):
         Takes data and a binning derived from the runlist. Bins the data,
         normalizes to a rate in HZ and fits a RateFunction over the whole
         time span to it. This function serves as a rate per time model.
+
 
         Parameters
         ----------
@@ -236,46 +332,18 @@ class RunlistBGRateInjector(BGRateInjector):
         """
         # Put data into run bins to fit them
         h = self._create_runtime_hist(T, self.goodrun_dict, remove_zero_runs)
+
         rate = h["rate"]
         rate_std = h["rate_std"]
         binmids = 0.5 * (h["start_mjd"] + h["stop_mjd"])
 
-        resx = self.rate_func.fit(binmids, rate, p0=None, rate_std=rate_std)
+        resx = self.rate_func.fit(binmids, rate, p0=x0, rate_std=rate_std,
+                                  **kwargs)
 
-        # Setup wrappers for functions with best fit pars plugged in
+        # Set wrappers for functions with best fit pars plugged in and livetime
+        # class variables as promised
+        self._livetime = np.sum(h["runtime"])
         return self._set_best_fit(resx)
-
-    @docs.dedent
-    def sample(self, t, trange, poisson=True, random_state=None):
-        """
-        %(BGRateInjector.sample.summary)s
-
-        Parameters
-        ----------
-        %(BGRateInjector.sample.parameters)s
-
-        Returns
-        -------
-        %(BGRateInjector.sample.returns)s
-        """
-        if self.best_pars is None:
-            raise RuntimeError("Injector was not fit to data yet.")
-
-        # BG expectations are the integrals over each time frames, shape (nsrcs)
-        t, trange = self._prep_t_trange(t, trange)
-        expect = self.get_nb(t, trange)
-        nsrcs = len(t)
-
-        # Get number of actual events to sample times for
-        if poisson:
-            nevents = np.random.poisson(lam=expect, size=nsrcs)
-        else:  # If one expectation is < 0.5 no event is sampled for that src
-            nevents = np.round(expect).astype(int)
-
-        # Sample all nevents for this trial from the rate function at once
-        return self.rate_func.sample(t, trange, self.best_pars,
-                                     n_samples=nevents,
-                                     random_state=random_state)
 
     def create_goodrun_dict(self, runlist, filter_runs):
         """
@@ -395,8 +463,69 @@ class RunlistBGRateInjector(BGRateInjector):
         a = np.vstack((run, rate, runtime, start_mjd, stop_mjd, evts, rate_std))
         rate_rec = np.core.records.fromarrays(a, dtype=dtype)
 
-        # Final total livetime and store stuff
-        self.livetime = np.sum(rate_rec["runtime"])
-        self.rate_rec = rate_rec
-
+        self._rate_rec = rate_rec
         return rate_rec
+
+
+class BinnedBGRateInjector(BGRateInjector):
+    def __init__(self, rate_func):
+        """
+        Binned Background Rate Injector
+
+        Creates the time depending rate from already binned rates.
+
+        Parameters
+        ----------
+        %(BGRateInjector.init.parameters)s
+        """
+        super(BinnedBGRateInjector, self).__init__(rate_func)
+        return
+
+    @docs.dedent
+    def fit(self, tbins, rate, rate_std=None, x0=None, **kwargs):
+        """
+        %(BGRateInjector.fit.summary)s
+
+        Takes data and a binning derived from the runlist. Bins the data,
+        normalizes to a rate in HZ and fits a RateFunction over the whole
+        time span to it. This function serves as a rate per time model.
+
+        Parameters
+        ----------
+        tbins : array-like, shape (nruns, 2)
+            Explicit edges `[[start1, stop1], ..., [startN, stopN]]` of the
+            pre-created time bins in MJD days. First columns is start MJD,
+            second column is stop MJD for each run.
+        rate : array-like, shape (nruns)
+            Rates at given times `t` in Hz.
+        rate_std : array-like, shape(nruns), optional
+            Standard deviations for each datapoint. If None, all are set to 1.
+            If rate_std is a good description of the standard deviation, then
+            the fit statistics follows a :math:`\chi^2` distribution. But for a
+            binned fit this makes less sense, because low standard deviation
+            means low statistics, so better use unweighted. (default: None)
+        x0 : array-like, optional
+            Seed values for the fit function as described above. If None,
+            defaults from `RateFunction` are used. (default: None)
+        kwargs
+            Other arguments are passed to the `scipy.optimize.minimize` method.
+
+        Returns
+        -------
+        best_estimator : function
+            Rate function with the best fit parameters plugged in.
+        """
+        if tbins.shape != (len(rate), 2):
+            raise ValueError("`tbins` must have shape (nruns, 2) and the " +
+                             "same length as `rate`.")
+
+        start_mjd = tbins[:, 0]
+        stop_mjd = tbins[:, 1]
+        binmids = 0.5 * (start_mjd + stop_mjd)
+        resx = self.rate_func.fit(binmids, rate, p0=x0, rate_std=rate_std,
+                                  **kwargs)
+
+        # Set wrappers for functions with best fit pars plugged in and livetime
+        # class variables as promised
+        self._livetime = np.sum(stop_mjd - start_mjd)
+        return self._set_best_fit(resx)
