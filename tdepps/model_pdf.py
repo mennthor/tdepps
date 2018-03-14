b1407f65671debc41ea4c2f2bd35062a52f9f470
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-14 18:18:09 +0100

    model_pdf.py :: Abstract base class

--- tdepps/model_pdf.py
+++ tdepps/model_pdf.py
@@ -1,10 +1,25 @@
 # coding: utf-8
 
+import abc
 
-class GRBInjectionModel(object):
+
+class PDF(object):
+    """ Interface for LLH PDF type classes. """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get_soverb():
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_args():
+        raise NotImplementedError
+
+
+class GRBPDF(PDF):
     """
-    Models the PDF part for the LLH function. Implements: `get_soverb()` and
-    `get_args()`.
+    Models the PDF part for the GRB LLH function. Implements: ``get_soverb()``
+    and ``get_args()``.
     """
     def __init__(self, X, MC, srcs):
         """
