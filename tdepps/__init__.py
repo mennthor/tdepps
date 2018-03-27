eee6dc35f97458a911927f51bd69a1aa9ef2b06e
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-27 19:29:54 +0200

    __init__.py :: Remove top level imports for now.

--- tdepps/__init__.py
+++ tdepps/__init__.py
@@ -5,7 +5,5 @@ Top level contains main code with LLH and Injectors build from building blocks
 in the toolkit sub module.
 """
 
-from .analysis import GRBLLHAnalysis
-from .model_injection import GRBModelInjector, MultiGRBModelInjector
-from .model_pdf import GRBPDF
-from .llh import GRBLLH, MultiGRBLLH
+from __future__ import absolute_import
+
