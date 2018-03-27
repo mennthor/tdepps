64dec202a5b9052129bb82ce8819efa43d032bb2
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-27 15:27:22 +0200

    Module imports in the inits

--- tdepps/__init__.py
+++ tdepps/__init__.py
@@ -4,3 +4,8 @@
 Top level contains main code with LLH and Injectors build from building blocks
 in the toolkit sub module.
 """
+
+from .analysis import GRBLLHAnalysis
+from .model_injection import GRBModelInjector, MultiGRBModelInjector
+from .model_pdf import GRBPDF
+from .llh import GRBLLH, MultiGRBLLH
--- tdepps/toolkit/__init__.py
+++ tdepps/toolkit/__init__.py
@@ -4,3 +4,10 @@
 Collection of higher level classes and methods to build a specific injector
 or PDF model class.
 """
+
+from .model_toolkit import (SignalFluenceInjector, HealpySignalFluenceInjector,
+                            MultiSignalFluenceInjector)
+from .model_toolkit import UniformTimeSampler
+from .model_toolkit import BGDataInjector, TimeDecDependentBGDataInjector
+from .model_toolkit import (SinusRateFunction, SinusFixedRateFunction,
+                            SinusFixedConstRateFunction, ConstantRateFunction)
