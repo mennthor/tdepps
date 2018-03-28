ce030234f915666ac426bbcde69819b21ca7c8d7
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-28 07:57:48 +0200

    utils/__init__.py :: Fixed global imports

--- tdepps/utils/__init__.py
+++ tdepps/utils/__init__.py
@@ -7,8 +7,9 @@ directly in the top level code.
 
 from __future__ import absolute_import
 
-from .coords import ThetaPhiToDecRa, cos_angdist, rotator
-from .io import arr2str, fill_dict_defaults, logger
+from .coords import thetaphi2decra, cos_angdist, rotator
+from .io import arr2str, fill_dict_defaults, logger, create_run_dict
+from .phys import power_law_flux, make_rate_records, rebin_rate_rec
 from .spline import (make_spl_edges, fit_spl_to_hist, get_stddev_from_scan,
                      spl_normed, make_time_dep_dec_splines)
-from .stats import random_choice, weighted_cdf, delta_chi2
+from .stats import random_choice, weighted_cdf, delta_chi2, fit_chi2_cdf
