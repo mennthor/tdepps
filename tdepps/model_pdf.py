03f12c640125b45d87e1df97b7fd1fc3cde5c12a
Thorben Menne <mennthor@aol.com>
2018-03-28 15:03:24 +0200

    model_pdf.py :: Adapted model pdf class to hold needed data names and give names of provided args

--- tdepps/model_pdf.py
+++ tdepps/model_pdf.py
@@ -2,11 +2,24 @@
 
 from __future__ import absolute_import
 import abc
+import numpy as np
 
 
 class BasePDF(object):
     """ Interface for unbinned LLH PDF type classes """
     __metaclass__ = abc.ABCMeta
+
+    _needed_data = None
+
+    @abc.abstractproperty
+    def needed_data(self):
+        """ Data recarray attributes this PDF model needs for evaluation """
+        pass
+
+    @abc.abstractproperty
+    def provided_args(self):
+        """ Additional LLH arguments this model provides via `get_args` """
+        pass
 
     @abc.abstractmethod
     def get_soverb(self):
@@ -31,10 +44,28 @@ class GRBPDF(BasePDF):
         Build PDFs that decsribe BG and/or signal-like events in the LLH.
         The LLH PDFs must not necessarily match the injected data model.
         """
-        return
+        self._needed_data = np.array(
+            ["timeMJD", "dec", "ra", "sigma", "logE"])
+        self._provided_args = ["src_w", "nb"]
+
+    @property
+    def needed_data(self):
+        return self._needed_data
+
+    @property
+    def provided_args(self):
+        return self._provided_args
 
     def get_args(self):
         pass
 
     def get_soverb(self):
         pass
+
+    def _select_X(self, X):
+        """
+        Select events in a band around the source declinations and discard those
+        outside, which have a negligible contribution on the the result.
+        """
+        # TODO
+        return X
