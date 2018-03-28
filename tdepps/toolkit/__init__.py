171d470f9403907562a13f946e393fa78408793f
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-28 08:00:55 +0200

    toolkit/__init__.py :: Fixed BGInjector imports

--- tdepps/toolkit/__init__.py
+++ tdepps/toolkit/__init__.py
@@ -10,6 +10,7 @@ from __future__ import absolute_import
 from .model_toolkit import (SignalFluenceInjector, HealpySignalFluenceInjector,
                             MultiSignalFluenceInjector)
 from .model_toolkit import UniformTimeSampler
-from .model_toolkit import BGDataInjector, TimeDecDependentBGDataInjector
+from .model_toolkit import (ResampleBGDataInjector, Binned3DBGDataInjector,
+                            TimeDecDependentBGDataInjector, KDEBGDataInjector)
 from .model_toolkit import (SinusRateFunction, SinusFixedRateFunction,
                             SinusFixedConstRateFunction, ConstantRateFunction)
