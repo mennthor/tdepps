c73f779d2aeb5030f2952b34193b0edea4e645ec
Thorben Menne <mennthor@aol.com>
2018-03-29 15:34:04 +0200

    toolkit/__init__.py :: Removed imports from deleted, old bg injectors

--- tdepps/toolkit/__init__.py
+++ tdepps/toolkit/__init__.py
@@ -10,8 +10,6 @@ from __future__ import absolute_import
 from .model_toolkit import (SignalFluenceInjector, HealpySignalFluenceInjector,
                             MultiSignalFluenceInjector)
 from .model_toolkit import UniformTimeSampler
-from .model_toolkit import (ResampleBGDataInjector, Binned3DBGDataInjector,
-                            TimeDecDependentBGDataInjector, KDEBGDataInjector,
-                            MultiBGDataInjector)
+from .model_toolkit import TimeDecDependentBGDataInjector, MultiBGDataInjector
 from .model_toolkit import (SinusRateFunction, SinusFixedRateFunction,
                             SinusFixedConstRateFunction, ConstantRateFunction)
